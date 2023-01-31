1c5d8ea749b2387d045a831fd9344a4257a52ba8
AdamRazo <122961294+AdamRazo@users.noreply.github.com>
2023-01-30 19:16:38 -0700

    did some things

--- python_strings.py
+++ python_strings.py
@@ -2,33 +2,48 @@
 #   - Create the following variables
 #   - my_first_name
 #       -set this equal to your first name
+my_first_name = 'Adam'
+
 #   - my_last_name
 #       -set this equal to your last name
+my_last_name = 'Razo'
+
 #   - my_year_of_birth
+my_year_of_birth = '2002'
+
 #       -set this equal to your birth year (doesn't have to be real should be less then 100 yrs ago)
 #   - current_year
 #       -set this equal to 2020
-
+Current_year = '2023'
 
 
 
 # TODO String Indexing
+
+
 #   - Print the following items (one per line) (print using variables)
 #       - first name  
+print(my_first_name)
 #       - last name
+print(my_last_name)
 #       - first letter of your first name (use the +index)
+print(my_first_name[0])
 #       - second letter of your last name (use the -index)
+print(my_last_name[-2])
 #       - first two letter of your first name (use the +index)
+print(my_last_name[0:2])
 #       - second two letter of your last name (use the -index)
-
+print(my_last_name[-2:])
 
 
 
 #TODO Combining Strings
 #   - Print the following items (one per line) (print using variables)
 #       -first name and last name combined
+print(my_first_name + my_last_name)
+print(my_first_name, my_last_name)
 #       -first name six times
-
+print(my_first_name*6)
 
 
 
@@ -36,8 +51,9 @@
 # TODO Formatting Strings
 #   - Print the following items (one per line) (print using variables)
 #       - first name last name -was born in- year of birth
+print(my_first_name, my_last_name,'was born in', my_year_of_birth)
 #       - first name last name -was born in- year of birth. first name -enjoyed celebrating- current year
-
+print(my_first_name, my_last_name, 'was born in', my_year_of_birth, '.', my_first_name, 'enjoyed celebrating', Current_year)
 
 
 # TODO Escape characters
